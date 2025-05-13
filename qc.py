13d05a5d799fe4dd0292093f784f3d58bb14193a
ShiyuZhang <shiyuzhang2003@gmail.com>
2025-05-13 15:16:40 -0400

    initial commit

---
+++ qc.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class State:
+    def __init__(self):
+        self.state = 
+
+class Entanglement:
+    def __init__(self):
+        
+        
+def purify(state1, state2):
+    purified_state = State()
+    return 
