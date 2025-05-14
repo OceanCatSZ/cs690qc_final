c11804d8eb01151147be38b3a0047f2924ecd71b
ShiyuZhang <shiyuzhang2003@gmail.com>
2025-05-13 21:01:26 -0400

    qc changes

--- qc.py
+++ qc.py
@@ -1,18 +1,29 @@
 import numpy as np
-from qutip import basis, ket2dm, tensor, fidelity, qeye
+from qutip import *
 
 class Node:
     def __init__(self, name):
         self.name = name
-        self.neighbors = {}  # adjacent nodes
+        self.prev = None
+        self.next = None  # adjacent nodes
+        self.prevdist = 0
+        self.nextdist = 0
 
-    def connect(self, neighbor, distance):
-        self.neighbors[neighbor] = distance
-        neighbor.neighbors[self] = distance
+    def set_next(self, node, distance):
+        self.next = node
+        self.nextdist = distance
+        node.prev = self
+        node.prevdist = distance
+    
+    def set_prev(self, node, distance):
+        self.prev = node
+        self.prevdist = distance
+        node.next = self
+        node.nextdist = distance
 
 
 class Entanglement:
-    def __init__(self, node1: Node, node2: Node, fidelity: float):
+    def __init__(self, node1: Node, node2: Node, fidelity: float, T_depol: float):
         self.node1 = node1
         self.node2 = node2
         self.fidelity = fidelity
@@ -24,11 +35,15 @@ class Entanglement:
         # Construct Werner state: ρ = F |Φ⁺⟩⟨Φ⁺| + (1 - F) * (I/4)
         identity = tensor(qeye(2), qeye(2))
         self.rho = fidelity * self.phi_plus_dm + (1 - fidelity) * (identity / 4)
+        self.T_depol = T_depol
 
     def calFid(self, target_state=None):
         if target_state is None:
             target_state = self.phi_plus_dm
         return fidelity(self.rho, target_state)
+    
+    def decohere(self, t):
+        
 
 def purify(state1, state2):
     return 
@@ -45,21 +60,21 @@ def build_uniform_chain(L_total, N_repeaters=4):
     for i in range(len(node_names) - 1):
         node1 = nodes[node_names[i]]
         node2 = nodes[node_names[i + 1]]
-        node1.connect(node2, distance=link_distance)
+        node1.set_next(node2, distance=link_distance)
 
     return nodes
 
-def generate_entanglement_BK(node1, node2, L_att=22.5, tau_attempt=1.0):
+def generate_next_entanglement_BK(node1, node2, L_att=22.5, tau_attempt=1.0):
     """
     Attempts to generate an entangled pair between two *connected* nodes via the BK scheme.
     Returns (Entanglement, time_taken) if successful.
     Raises an error if the nodes are not direct neighbors.
     """
     # Ensure bidirectional connection
-    if node2 not in node1.neighbors or node1 not in node2.neighbors:
-        raise ValueError(f"{node1.name} and {node2.name} are not connected as neighbors.")
+    # if node2 not in node1.neighbors or node1 not in node2.neighbors:
+    #     raise ValueError(f"{node1.name} and {node2.name} are not connected as neighbors.")
 
-    distance = node1.neighbors[node2]/2
+    distance = node1.nextdist/2
     eta = np.exp(-distance / L_att)
     p_success = eta**2/2
 
@@ -80,7 +95,7 @@ def main():
     L_att = 22.5
 
     nodes = build_uniform_chain(L_total)
-    # print(nodes)
+    print(nodes)
     return
 
 if __name__ == '__main__':
